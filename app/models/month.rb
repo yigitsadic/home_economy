9e3e3df55f88792a647d8f954c62646664ce6462
Yigit Sadic <yigitsadic@gmail.com>
2024-06-29 11:17:59 +0300

    Reverted Activity model
    I didn't like how I model it.

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,7 +1,5 @@
 class Month < ApplicationRecord
   validates :name, :year, presence: true
   validates :name, uniqueness: { scope: :year }
-
-  has_many :activies
 end
 
--- db/migrate/20240624200213_add_financial_activity_fields_to_months.rb
+++
@@ -1,7 +0,0 @@
-class AddFinancialActivityFieldsToMonths < ActiveRecord::Migration[7.1]
-  def change
-    add_column :months, :income_total, :decimal, precision: 10, scale: 2, default: 0, null: false
-    add_column :months, :expense_total, :decimal, precision: 10, scale: 2, default: 0, null: false
-  end
-end
-
